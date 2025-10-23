c4758ed1119b5ba8410d0d2e6c5c90f76146577d
Romain Lefeuvre <romain.lefeuvre@inria.fr>
2025-10-23 08:29:20 +0200

    add  progress bar for filter

--- src/sampling_mining_workflows_dsl/operator/selection/filter/FilterOperator.py
+++ src/sampling_mining_workflows_dsl/operator/selection/filter/FilterOperator.py
@@ -1,6 +1,7 @@
 from sampling_mining_workflows_dsl.constraint.Constraint import Constraint
 from sampling_mining_workflows_dsl.element.Set import Set
 from sampling_mining_workflows_dsl.operator.Operator import Operator
+from tqdm import tqdm
 
 
 class FilterOperator(Operator):
@@ -11,9 +12,14 @@ class FilterOperator(Operator):
 
     def execute(self):
         self._output = Set()
-        for element in self._input.get_elements():
-            if self._constraint.is_satisfied(element):
-                self._output.add_element(element)
+        elements = self._input.get_elements()
+        
+        # Add progress bar for filtering operation
+        with tqdm(elements, desc="Filtering elements", unit="element") as pbar:
+            for element in pbar:
+                if self._constraint.is_satisfied(element):
+                    self._output.add_element(element)
+                    
         super().execute()
         return self
 
